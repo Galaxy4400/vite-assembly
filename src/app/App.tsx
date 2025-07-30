4b16952beefc4e434864c5210de9751378ca8e26
Moiseev Evgeny <moiseevEO@yandex.ru>
2025-07-30 19:20:03 +0300

    commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,3 @@
-import { useState } from 'react';
-
-import css from './App.module.scss';
-
 export const App = () => {
-	const [count, setCount] = useState(0);
-
-	return (
-		<div>
-			<div className={css['container']}>
-				<h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis, aut.</h1>
-			</div>
-		</div>
-	);
+	return <div></div>;
 };
